b12b3f3e6cdea396b23581b16978b8b6e10af283
zhangkaizhao <zhangkaizhao@gmail.com>
2016-01-21 01:51:40 +0800

    Unix-like rcs: jdk and openjdk

---
+++ rcs/jdk-jre8.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+export JAVA_HOME=/usr/lib/jvm/java-8-jdk/jre
+export PATH=/usr/lib/jvm/java-8-jdk/jre/bin/:$PATH
+# exec /path/to/application
---
+++ rcs/openjdk.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+export JAVA_HOME=/usr/lib/jvm/java-8-openjdk
+export PATH=/usr/lib/jvm/java-8-openjdk/bin/:$PATH
+# exec /path/to/application
