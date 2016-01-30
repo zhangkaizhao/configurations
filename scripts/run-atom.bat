553b469a2164450a77167aedd5eede5895239c12
zhangkaizhao <zhangkaizhao@gmail.com>
2016-01-31 02:59:41 +0800

    Windows scripts: go

--- scripts/run-atom.bat
+++ scripts/run-atom.bat
@@ -3,4 +3,5 @@ call D:\scripts\set-ruby.bat
 call D:\scripts\set-elixir.bat
 call D:\scripts\set-rust.bat
 call D:\scripts\set-php.bat
+call D:\scripts\set-go.bat
 atom
---
+++ scripts/set-go.bat
@@ -0,0 +1,6 @@
+::set GOOS=windows
+::set GOARCH=amd64
+set GOROOT=D:\programs\go
+::set GOBIN=%GOROOT%\bin
+set GOPATH=D:\goext
+set PATH=%GOPATH%\bin;%GOROOT%\bin;%PATH%
