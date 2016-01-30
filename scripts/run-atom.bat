7e4b30674a2a603669bcdea1d73022af4a44e745
zhangkaizhao <zhangkaizhao@gmail.com>
2016-01-31 02:11:52 +0800

    Windows scripts: php

--- scripts/run-atom.bat
+++ scripts/run-atom.bat
@@ -2,4 +2,5 @@ call D:\scripts\set-py35.bat
 call D:\scripts\set-ruby.bat
 call D:\scripts\set-elixir.bat
 call D:\scripts\set-rust.bat
+call D:\scripts\set-php.bat
 atom
---
+++ scripts/set-php.bat
@@ -0,0 +1,1 @@
+set PATH=D:\programs\php;%PATH%
