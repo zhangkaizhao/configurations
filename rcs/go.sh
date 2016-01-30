5bc6b31c0616ca69f750315ca24e92e00c51d5e5
zhangkaizhao <zhangkaizhao@gmail.com>
2016-01-30 23:49:02 +0800

    Unix-like rcs: update go

--- rcs/go.sh
+++ rcs/go.sh
@@ -1,6 +1,6 @@
 #export GOOS=linux
 #export GOARCH=amd64
 export GOROOT=$HOME/go
-export GOBIN=$GOROOT/bin
-export PATH=$GOBIN:$PATH
+#export GOBIN=$GOROOT/bin
 export GOPATH=$HOME/goext
+export PATH=$GOPATH/bin:$GOROOT/bin:$PATH
