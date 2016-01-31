01f80297c669b50d4157ad3aad0840d2f6ab59ad
zhangkaizhao <zhangkaizhao@gmail.com>
2016-01-31 22:35:08 +0800

    Windows scripts: cargo instead of rust

--- scripts/run-atom.bat
+++ scripts/run-atom.bat
@@ -1,8 +1,8 @@
 call D:\scripts\set-py35.bat
 call D:\scripts\set-ruby.bat
 call D:\scripts\set-elixir.bat
-call D:\scripts\set-rust.bat
 call D:\scripts\set-php.bat
 call D:\scripts\set-go.bat
 call D:\scripts\set-stack.bat
+call D:\scripts\set-cargo.bat
 atom
---
+++ scripts/set-cargo.bat
@@ -0,0 +1,4 @@
+rem environment variables: http://doc.crates.io/environment-variables.html
+set CARGO_HOME=D:\cargo_home
+set PATH=%CARGO_HOME%\bin;%PATH%
+::set PATH=%USERPROFILE%\.cargo\bin;%PATH%
