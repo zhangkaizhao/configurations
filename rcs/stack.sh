bf0bf7240d9d4aac0e0b76528b98c61a1a00f281
Kaizhao Zhang <zhangkaizhao@gmail.com>
2018-10-19 20:22:00 +0800

    Unix-like rcs: bash completion of Haskell stack

--- rcs/stack.sh
+++ rcs/stack.sh
@@ -1,3 +1,6 @@
 # Haskell Stack
 export STACK_ROOT=/zdata/moredata/stack
 export PATH=$HOME/.local/bin:$PATH
+
+# https://docs.haskellstack.org/en/stable/install_and_upgrade/#shell-auto-completion
+eval "$(stack --bash-completion-script stack)"
