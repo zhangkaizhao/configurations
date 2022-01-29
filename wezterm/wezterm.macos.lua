ce0068c5ebe57dee17b7af47ba94ebe9298470b4
Kaizhao Zhang <zhangkaizhao@gmail.com>
2022-01-29 21:39:09 +0800

    wezterm: no window close confirmations and more
    * no window close confirmations
    * better text background opacity

--- wezterm/wezterm.macos.lua
+++ wezterm/wezterm.macos.lua
@@ -3,9 +3,10 @@ local wezterm = require 'wezterm';
 return {
     enable_tab_bar = false,
     hide_tab_bar_if_only_one_tab = false,
+    window_close_confirmation = "NeverPrompt",
 
     window_background_opacity = 0.8,
-    text_background_opacity = 0.3,
+    text_background_opacity = 0.8,
 
     font = wezterm.font_with_fallback({
         "Inconsolata",
