e0656015b3fdb14d8868857cc9b91623c0fe1a4d
Kaizhao Zhang <zhangkaizhao@gmail.com>
2025-06-03 02:29:19 +0800

    ruby: fix a typo in rubocop config and other minor changes

--- macos/setup/gems.rb
+++ macos/setup/gems.rb
@@ -1,11 +1,11 @@
 # install: gem install -g gems.rb
+# upgrade: gem update -g gems.rb
 gem "cocoapods"
 gem "debug"
 gem "jekyll"
 gem "jekyll-import"
 gem "pry"
 gem "rubocop"
-gem "rucoa"
 gem "solargraph"
 gem "standard"
 gem "typeprof"
